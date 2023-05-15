0a618fa5158e86e4935b45a92abc2ad4ea5df278
Gaarrv <132135644+Gaarrv@users.noreply.github.com>
2023-05-15 19:47:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// window.onload = function() {
+//   var bannerContainer = document.getElementById("banner-container");
+//   var bannerWidth = bannerContainer.offsetWidth;
+
+//   // Duplicate the banner contents
+//   var bannerContents = bannerContainer.innerHTML;
+//   bannerContainer.innerHTML += bannerContents;
+
+//   // Set the width of the banner container to twice the original width
+//   bannerContainer.style.width = bannerWidth * 2 + "px";
+// }
+
+window.addEventListener("scroll", function() {
+    var img = document.getElementById("main-ramen");
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  
+    img.style.transform = "rotate(" + scrollTop/6 + "deg)";
+  });
